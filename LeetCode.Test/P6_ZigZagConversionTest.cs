549f33e808d6bea92a7110593824ca24536380d1
Zhijie Li <ZhijieLi@outlook.com>
2018-04-23 21:58:11 +0800

    P6

---
+++ LeetCode.Test/P6_ZigZagConversionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P6_ZigZagConversionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var s = "PAYPALISHIRING";
+            var numRows = 3;
+            var expected = "PAHNAPLSIIGYIR";
+            var t = new P6_ZigZagConversion();
+            var actual = t.Convert(s, numRows);
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            var s = "A";
+            var numRows = 1;
+            var expected = "A";
+            var t = new P6_ZigZagConversion();
+            var actual = t.Convert(s, numRows);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ LeetCode/P6_ZigZagConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P6_ZigZagConversion
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (s == null || numRows < 2)
+                return s;
+
+            int i = 0;
+            int j = 0;
+            var result = new StringBuilder();
+
+            for (; i < numRows; i++)
+            {
+                for (j = 0; j * (2 * numRows - 2) + i < s.Length; j++)
+                {
+                    result.Append(s[j * (2 * numRows - 2) + i]);
+                    if (i != 0 && i != numRows - 1 && (j + 1) * (2 * numRows - 2) - i < s.Length)
+                    {
+                        result.Append(s[(j + 1) * (2 * numRows - 2) - i]);
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
