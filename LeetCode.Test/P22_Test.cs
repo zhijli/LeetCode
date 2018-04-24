95e4223ce0f45668f04a6c0e3beede6cde99dec7
Zhijie Li <ZhijieLi@outlook.com>
2018-04-25 01:33:02 +0800

    P22

---
+++ LeetCode.Test/P22_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P22_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P22();
+            var result = t.GenerateParenthesis(4);
+
+            foreach(var str in result)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ LeetCode/P22.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P22
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var results = new List<string>();
+            var stack = new Stack<char>();
+            GenerateParenthesis(stack, n, results, "");
+            return results;
+        }
+
+        private void GenerateParenthesis(Stack<char> stack, int n, List<string> results, string result)
+        {
+            if (n == 0 && stack.Count == 0)
+            {
+                results.Add(result);
+            }
+
+            if (n > 0)
+            {
+                stack.Push('(');
+                GenerateParenthesis(new Stack<char>(stack), n - 1, results, result + '(');
+                stack.Pop();
+            }
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+                GenerateParenthesis(new Stack<char>(stack), n, results, result + ')');
+            }
+        }
+    }
+}
