b05cf39b53b337c44e552f6d5fb9a81adb1cb2a4
Zhijie Li <ZhijieLi@outlook.com>
2018-04-24 21:13:36 +0800

    P20

---
+++ LeetCode/P19.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P19
+    {
+        public class Solution
+        {
+
+            public class ListNode
+            {
+                public int val;
+                public ListNode next;
+                public ListNode(int x) { val = x; }
+            }
+
+
+            public ListNode RemoveNthFromEnd(ListNode head, int n)
+            {
+                if (head == null) return null;
+                if (n == 0) return head;
+
+                ListNode current, nextN;
+                current = nextN = head;
+                while (nextN.next != null && n > 0)
+                {
+                    nextN = nextN.next;
+                    n--;
+                }
+
+                if (n == 1)
+                {
+                   return head.next;
+                }
+
+                while (nextN.next != null)
+                {
+                    nextN = nextN.next;
+                    current = current.next;
+                }
+
+                current.next = current.next.next;
+                return head;
+            }
+        }
+    }
+}
---
+++ LeetCode/P20.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P20
+    {
+
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (c == '{' || c == '[' || c == '(')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        var c2 = stack.Pop();
+                        if (!((c == '}' && c2 == '{') || (c == ']' && c2 == '[') || (c == ')' && c2 == '(')))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
