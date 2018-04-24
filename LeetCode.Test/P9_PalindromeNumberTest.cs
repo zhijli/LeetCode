0851bea5495430244fb857fdc150dd8143d2a980
Zhijie Li <ZhijieLi@outlook.com>
2018-04-24 09:17:23 +0800

    P9

---
+++ LeetCode.Test/P9_PalindromeNumberTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P9_PalindromeNumberTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P9_PalindromeNumber();
+            t.IsPalindrome(12321);
+        }
+    }
+}
---
+++ LeetCode/P9_PalindromeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P9_PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            var str = x.ToString();
+            int i = 0;
+            int j = str.Length - 1;
+            while (i <= j && str[i] == str[j])
+            {
+                i++;
+                j--;
+            }
+            if (i > j)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
