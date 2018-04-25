75a34b9279f6e996c866687298d8d4c579385424
Zhijie Li <ZhijieLi@outlook.com>
2018-04-25 17:28:52 +0800

    P29

---
+++ LeetCode.Test/P29_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P29_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P29();
+            Assert.AreEqual(t.Divide(123456, 3), 123456/3);
+            Assert.AreEqual(t.Divide(123456, -3), 123456 / -3);
+            Assert.AreEqual(t.Divide(-123456, 3), -123456 / 3);
+            Assert.AreEqual(t.Divide(-123456, -3), -123456 / -3);
+        }
+    }
+}
---
+++ LeetCode/P29.cs
@@ -0,0 +1,72 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class P29
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == Int32.MinValue && divisor == -1)
+                return Int32.MaxValue;
+            if (dividend == Int32.MinValue && divisor == 1)
+                return Int32.MinValue;
+
+            if (dividend > 0 && divisor > 0)
+            {
+                return DivideCore(-dividend, -divisor);
+            }
+            if (dividend < 0 && divisor > 0)
+            {
+                int value = this.DivideCore(dividend, -divisor);
+                return -this.DivideCore(dividend, -divisor);
+            }
+            if (dividend > 0 && divisor < 0)
+            {
+                return -this.DivideCore(-dividend, divisor);
+            }
+            if (dividend < 0 && divisor < 0)
+            {
+                return this.DivideCore(dividend, divisor);
+            }
+
+            return 0;
+        }
+
+        private int DivideCore(int dividend, int divisor)
+        {
+            var scale = 1;
+            var bounder = int.MinValue >> 1;
+
+            while (bounder <= divisor && (dividend <= (divisor << 1)))
+            {
+                scale <<= 1;
+                divisor <<= 1;
+            }
+
+            int result = 0;
+
+            while (scale > 0)
+            {
+                if (dividend <= divisor)
+                {
+                    dividend -= divisor;
+                    result += scale;
+                }
+                divisor >>= 1;
+                scale >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
