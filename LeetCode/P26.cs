52850db9da21a7cdb8148985a036068b487ed87d
Zhijie Li <ZhijieLi@outlook.com>
2018-04-25 15:04:49 +0800

    P26

---
+++ LeetCode/P26.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            var index = 0;
+            int currentNum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (currentNum != nums[i])
+                {
+                    nums[++index] = nums[i];
+                    currentNum = nums[i];
+                }
+            }
+            return index + 1;
+        }
+    }
+}
