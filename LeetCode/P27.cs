f9eca63ea6c31c02ec4f0309ecc5ee2054581db3
Zhijie Li <ZhijieLi@outlook.com>
2018-04-25 15:26:12 +0800

    P27

---
+++ LeetCode/P27.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null) return 0;
+
+            int j = nums.Length - 1;
+
+            while (j >= 0 && nums[j] == val)
+            {
+                j--;
+            }
+
+            for (int i = j - 1; i >= 0; i--)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[j];
+                    nums[j] = val;
+                    j--;
+                }
+            }
+
+            return j + 1;
+        }
+    }
+}
