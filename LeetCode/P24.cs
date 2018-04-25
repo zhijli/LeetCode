16a66a32fa27140e9b709e358c5417fe77c68013
Zhijie Li <ZhijieLi@outlook.com>
2018-04-25 14:40:02 +0800

    P24 25

---
+++ LeetCode/P24.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P24
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            var current = head;
+            var result = head.next;
+            ListNode pre = null;
+
+            while (current != null && current.next != null)
+            {
+                var next = current.next;
+                current.next = next.next;
+                if (pre != null)
+                {
+                    pre.next = next;
+                }
+                next.next = current;
+                pre = current;
+                current = current.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/P25.cs
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class P25
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            var stack = new Stack<ListNode>();
+            var current = head;
+            var result = head;
+            ListNode pre = null;
+            while (current != null)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    if (current != null)
+                    {
+                        stack.Push(current);
+                        current = current.next;
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+
+                for (int i = 0; i < k; i++)
+                {
+                    var node = stack.Pop();
+                    if (pre != null)
+                    {
+                        pre.next = node;
+                    }
+                    else
+                    {
+                        result = node;
+                    }
+                    pre = node;
+                }
+                pre.next = current;
+            }
+            return result;
+        }
+    }
+}
