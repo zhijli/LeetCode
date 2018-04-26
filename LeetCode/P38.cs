77d0bd96f164e4bd20f3ff0989fc4ac172b17b98
Zhijie Li <ZhijieLi@outlook.com>
2018-04-26 17:09:26 +0800

    P38

---
+++ LeetCode/P38.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlTypes;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P38
+    {
+        public string CountAndSay(int n)
+        {
+            var current = "1";
+            string next = null;
+
+            for (int i = 1; i < n; i++)
+            {
+                next = GetNext(current);
+                current = next;
+            }
+
+            return current;
+        }
+
+        private string GetNext(string str)
+        {
+            var result = new StringBuilder();
+            var count = 1;
+            char current = str[0];
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != current)
+                {
+                    result.Append(count.ToString());
+                    result.Append(current);
+                    current = str[i];
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            result.Append(count.ToString());
+            result.Append(current);
+
+            return result.ToString();
+        }
+    }
+}
