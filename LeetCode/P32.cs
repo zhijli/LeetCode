1878a37364ac861fd4203364126c5cc025f0aff5
Zhijie Li <ZhijieLi@outlook.com>
2018-04-26 14:39:31 +0800

    P32

---
+++ LeetCode/P32.cs
@@ -0,0 +1,80 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    class P32
+    {
+        public int LongestValidParentheses(string s)
+        {
+            if (s == null || s.Length < 2) return 0;
+
+            var max = 0;
+            var longs = new int[s.Length];
+            longs[0] = 0;
+            if (s[0] == '(' && s[1] == ')')
+            {
+                longs[1] = 2;
+            }
+            else
+            {
+                longs[1] = 0;
+            }
+
+            for (int i = 2; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    longs[i] = 0;
+                }
+                else if (s[i - 1] == '(')
+                {
+                    longs[i] = longs[i - 2] + 2;
+                }
+                else if (i - 1 - longs[i - 1] >= 0)
+                {
+                    if (s[i - 1 - longs[i - 1]] == '(')
+                    {
+                        if (i - 1 - longs[i - 1] - 1 >= 0)
+                        {
+                            longs[i] = longs[i - 1] + 2 + longs[i - 1 - longs[i - 1] - 1];
+                        }
+                        else
+                        {
+                            longs[i] = longs[i - 1] + 2;
+                        }
+                    }
+                    else
+                    {
+                        longs[i] = 0;
+                    }
+
+                }
+                else
+                {
+                    longs[i] = 0;
+                }
+
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (max < longs[i])
+                {
+                    max = longs[i];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ LeetCode/P4.cs
@@ -0,0 +1,92 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P4
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int count = nums1.Length + nums2.Length;
+
+            if (count % 2 == 0)
+            {
+                return FindN(nums1, nums2, count / 2);
+            }
+            else
+            {
+                return (FindN(nums1, nums2, count / 2) + FindN(nums1, nums2, count / 2 + 1)) / 2.0;
+            }
+        }
+
+        private int FindN(int[] nums1, int[] nums2, int n)
+        {
+            int max = Math.Max(nums1[nums1.Length - 1], nums2[nums2.Length - 1]);
+            int min = Math.Min(nums1[0], nums2[0]);
+            int mid = (max + min) / 2;
+
+            var index = SearchInsert(nums1, nums2, mid);
+            while (index != n)
+            {
+                if (index > n)
+                {
+                    mid = (min + mid) / 2;
+                    index = SearchInsert(nums1, nums2, mid);
+                }
+                else
+                {
+                    mid = (max + mid) / 2;
+                    index = SearchInsert(nums1, nums2, mid);
+                }
+            }
+
+            return mid;
+        }
+
+        private int SearchInsert(int[] nums1, int[] nums2, int mid)
+        {
+            return SearchInsert(nums1, mid) + SearchInsert(nums2, mid);
+
+        }
+
+        private int SearchInsert(int[] nums, int target)
+        {
+            return SearchInsert(nums, 0, nums.Length - 1, target);
+        }
+
+        private int SearchInsert(int[] nums, int begin, int end, int target)
+        {
+            int mid = (begin + end) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] > target)
+            {
+                if (mid == begin)
+                {
+                    return mid;
+                }
+                return SearchInsert(nums, begin, mid - 1, target);
+            }
+            else
+            {
+                if (mid == end)
+                {
+                    return mid + 1;
+                }
+                return SearchInsert(nums, mid + 1, end, target);
+            }
+        }
+    }
+}
