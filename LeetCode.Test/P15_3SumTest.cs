291547754c4777b285d42291d1c3cc7096f198f0
Zhijie Li <ZhijieLi@outlook.com>
2018-04-29 02:44:05 +0800

    P15

--- LeetCode.Test/P15_3SumTest.cs
+++ LeetCode.Test/P15_3SumTest.cs
@@ -9,9 +9,16 @@ namespace LeetCode.Test
         [TestMethod]
         public void TestMethod1()
         {
-            int[] nums = { -1, 0, 1, 2, -1, -4 };
+
+            int[] nums = { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 };
             var t = new P15_3Sum();
-            t.ThreeSum(nums);
+            var result = t.ThreeSum(nums);
+
+            int[] nums1 = { 0, 0, 0 };
+            result = t.ThreeSum(nums1);
+
+            int[] nums2 = { -4, 1, -3, -4, 0, -5, -1, -1, 4, 4 };
+            result = t.ThreeSum(nums2);
         }
     }
 }
--- LeetCode/P15_3Sum.cs
+++ LeetCode/P15_3Sum.cs
@@ -17,47 +17,171 @@ namespace LeetCode
     {
         public IList<IList<int>> ThreeSum(int[] nums)
         {
+            if (nums == null || nums.Length == 0) return new List<IList<int>>();
+
+            var lists = nums.ToList();
+            lists.Sort();
+            nums = lists.ToArray();
+
+            var max = nums[nums.Length - 1];
+
             var result = new List<IList<int>>();
-            var dic = new Dictionary<int, List<int>>();
+            var dic = new Dictionary<int, int>();
 
             for (int i = 0; i < nums.Length; i++)
             {
                 if (dic.ContainsKey(nums[i]))
                 {
-                    dic[nums[i]].Add(i);
+                    dic[nums[i]] = i;
                 }
                 else
                 {
-                    dic.Add(nums[i], new List<int>() { i });
+                    dic.Add(nums[i], i);
                 }
             }
 
-            for (int i = 0; i < nums.Length; i++)
+            int right = nums.Length - 1;
+            for (int i = 0; i < nums.Length - 1; i++)
             {
+                if (i >= 1 && nums[i] == nums[i - 1]) continue;
+                if (nums[i] > 0) break;
+                //int left = FindRightIndex(nums, i + 1, nums.Length - 1, 0 - nums[i] - max);
                 for (int j = i + 1; j < nums.Length; j++)
                 {
                     var expected = 0 - nums[i] - nums[j];
-
                     if (dic.ContainsKey(expected))
                     {
-                        var indexList = dic[expected];
-
-                        foreach (var index in indexList)
+                        var index = dic[expected];
+                        if (index != i && index != j)
                         {
-                            if (index != i && index != j)
-                            {
-                                var temp = new List<int>() { nums[i], nums[j], nums[index] };
-                                temp.Sort();
-                                if (!result.Exists(item => item[0] == temp[0] && item[1] == temp[1] && item[2] == temp[2]))
-                                {
-                                    result.Add(temp);
-                                }
-                            }
+                            var temp = new List<int>() { nums[i], nums[j], nums[index] };
+                            result.Add(temp);
+                            right = index - 1;
                         }
                     }
                 }
             }
-            return result;
+
+            return result.Distinct(new MyEqualityComparer()).ToList();
+        }
+
+        private int FindRightIndex(int[] nums, int begin, int end, int target)
+        {
+            int mid = (begin + end) / 2;
+
+            while (begin < end - 1)
+            {
+                mid = (begin + end) / 2;
+
+                if (nums[mid] > target)
+                {
+                    end = mid;
+                }
+                if (nums[mid] < target)
+                {
+                    begin = mid + 1;
+                }
+                if (nums[mid] == target)
+                {
+                    begin = mid;
+                }
+            }
+
+            if (begin == end - 1)
+            {
+                if (nums[end] <= target)
+                {
+                    return end;
+                }
+                else if (nums[begin] >= target)
+                {
+                    return begin;
+                }
+                else
+                {
+                    return end;
+                }
+            }
+
+            return begin;
+        }
+
+        private int FindLeftIndex(int[] nums, int begin, int end, int target)
+        {
+            int mid = (begin + end) / 2;
+
+            while (begin < end - 1)
+            {
+                mid = (begin + end) / 2;
+
+                if (nums[mid] >= target)
+                {
+                    end = mid - 1;
+                }
+                if (nums[mid] < target)
+                {
+                    begin = mid;
+                }
+            }
+
+            if (begin == end - 1)
+            {
+                if (nums[begin] >= target)
+                {
+                    return begin;
+                }
+                else if (nums[end] <= target)
+                {
+                    return end;
+                }
+                else
+                {
+                    return begin;
+                }
+            }
+
+            return begin;
+        }
+    }
+
+    internal class MyEqualityComparer : IEqualityComparer<IList<int>>
+    {
+        public bool Equals(IList<int> x, IList<int> y)
+        {
+            if (x.Count == y.Count)
+            {
+                var x1 = x.ToList();
+                x1.Sort();
+                var y1 = y.ToList();
+                y1.Sort();
+                for (int i = 0; i < x1.Count; i++)
+                {
+                    if (x1[i] != y1[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode(IList<int> obj)
+        {
+            if (obj.Count > 0)
+            {
+                var result = obj[0].GetHashCode();
+
+                for (int i = 1; i < obj.Count; i++)
+                {
+                    result ^= obj[i];
+                }
+                return result;
+            }
+            return obj.GetHashCode();
         }
     }
 }
