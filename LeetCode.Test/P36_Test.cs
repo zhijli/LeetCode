df394cfbc4e9683156ec14fe95b934eac90380f4
Zhijie Li <ZhijieLi@outlook.com>
2018-04-29 16:49:52 +0800

    P36

---
+++ LeetCode.Test/P36_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P36_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P36();
+            char[,] board = { { '.', '.', '.', '.', '5', '.', '.', '1', '.' }, { '.', '4', '.', '3', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '3', '.', '.', '1' }, { '8', '.', '.', '.', '.', '.', '.', '2', '.' }, { '.', '.', '2', '.', '7', '.', '.', '.', '.' }, { '.', '1', '5', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '2', '.', '.', '.' }, { '.', '2', '.', '9', '.', '.', '.', '.', '.' }, { '.', '.', '4', '.', '.', '.', '.', '.', '.' } };
+            Assert.IsFalse(t.IsValidSudoku(board));
+        }
+    }
+}
---
+++ LeetCode/P36.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P36
+    {
+        public bool IsValidSudoku(char[,] board)
+        {
+            bool[] checker;
+
+            for (int row = 0; row < 9; row++)
+            {
+                checker = new bool[10];
+                for (int col = 0; col < 9; col++)
+                {
+                    if (board[row, col] != '.')
+                    {
+                        int val = board[row, col] - '0';
+                        if (checker[val] == true)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            checker[val] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int col = 0; col < 9; col++)
+            {
+                checker = new bool[10];
+                for (int row = 0; row < 9; row++)
+                {
+                    if (board[row, col] != '.')
+                    {
+                        int val = board[row, col] - '0';
+                        if (checker[val] == true)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            checker[val] = true;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                int centerRow = i * 3 + 1;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    int centerCol = j * 3 + 1;
+
+                    checker = new bool[10];
+                    for (int offsetRow = -1; offsetRow <= 1; offsetRow++)
+                    {
+                        for (int offsetCol = -1; offsetCol <= 1; offsetCol++)
+                        {
+                            if (board[centerRow + offsetRow, centerCol + offsetCol] != '.')
+                            {
+                                int val = board[centerRow + offsetRow, centerCol + offsetCol] - '0';
+                                if (checker[val] == true)
+                                {
+                                    return false;
+                                }
+                                else
+                                {
+                                    checker[val] = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
