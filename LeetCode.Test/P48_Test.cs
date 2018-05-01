a2da13099baf825c7144ae24861bc0ef40c6f468
Zhijie Li <ZhijieLi@outlook.com>
2018-05-01 14:12:55 +0800

    P48

---
+++ LeetCode.Test/P48_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P48_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P48();
+            t.Rotate(new[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+        }
+    }
+
+}
---
+++ LeetCode/P48.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P48
+    {
+        public void Rotate(int[,] matrix)
+        {
+            if (matrix == null || matrix.Length == 0) return;
+
+            var n = matrix.GetLength(0);
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = i; j < n - i - 1; j++)
+                {
+                    Rotate(matrix, n, i, j);
+                }
+            }
+        }
+
+        private void Rotate(int[,] matrix, int x, int i, int j)
+        {
+            int n = x - 1;
+            var temp = matrix[i, j];
+            matrix[i, j] = matrix[n - j, i];
+            matrix[n - j, i] = matrix[n - i, n - j];
+            matrix[n - i, n - j] = matrix[j, n - i];
+            matrix[j, n - i] = temp;
+        }
+    }
+}
