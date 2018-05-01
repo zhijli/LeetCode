0314307393a1e1772247a609060788ddf4a757e0
Zhijie Li <ZhijieLi@outlook.com>
2018-05-02 01:17:16 +0800

    P50 Temp

---
+++ LeetCode.Test/P50_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P50_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P50();
+            Assert.AreEqual(t.MyPow(2, 10), Math.Pow(2, 10));
+        }
+    }
+}
---
+++ LeetCode/P50.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P50
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return x;
+
+            if (n < 0)
+            {
+                if (n == int.MaxValue)
+                {
+                    return 1 / (MyPow(x, int.MaxValue) * x);
+                }
+
+                return 1 / MyPow(x, -n);
+            }
+            else
+            {
+                int a = 1;
+                var temp = n;
+                var result = x;
+                while (temp > 1)
+                {
+                    temp >>= 1;
+                    result *= result ;
+                    a <<= 1;
+                }
+                var b = n % a;
+                return result * MyPow(x, b);
+            }
+        }
+    }
+}
