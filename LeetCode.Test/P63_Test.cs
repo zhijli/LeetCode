fcf261a001134fab0ac3ffcd8312596a13bd45b8
Zhijie Li <ZhijieLi@outlook.com>
2018-05-02 02:15:18 +0800

    P63

---
+++ LeetCode.Test/P63_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P63_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P63();
+            t.UniquePathsWithObstacles(new int[,] {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
+        }
+    }
+}
---
+++ LeetCode/P63.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P63
+    {
+        public int UniquePathsWithObstacles(int[,] obstacleGrid)
+        {
+            int row = obstacleGrid.GetLength(0);
+            int col = obstacleGrid.GetLength(1);
+            int[,] dp = new int[row, col];
+
+            if (obstacleGrid[row - 1, col - 1] == 0)
+            {
+                dp[row - 1, col - 1] = 1;
+            }
+
+            for (int i = row - 1; i >= 0; i--)
+            {
+                for (int j = col - 1; j >= 0; j--)
+                {
+                    if (obstacleGrid[i, j] == 0)
+                    {
+                        if (i < row - 1 && obstacleGrid[i + 1, j] == 0)
+                        {
+                            dp[i, j] += dp[i + 1, j];
+                        }
+                        if (j < col - 1 && obstacleGrid[i, j + 1] == 0)
+                        {
+                            dp[i, j] += dp[i, j + 1];
+                        }
+                    }
+                }
+            }
+
+            return dp[0, 0];
+        }
+    }
+}
