1a7d3e69382d65a3dc6e999d8833d2c5ae2e51fe
Zhijie Li <ZhijieLi@outlook.com>
2018-05-01 16:05:29 +0800

    P53

---
+++ LeetCode/P53.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P53
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int max = nums[0];
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (dp[i - 1] < 0)
+                {
+                    dp[i] = nums[i];
+                }
+                else
+                {
+                    dp[i] = dp[i - 1] + nums[i];
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (max < dp[i]) max = dp[i];
+            }
+            return max;
+        }
+    }
+}
