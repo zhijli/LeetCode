b9fbf4df0dbf2f3b46de35fbe886e857be2a4178
Zhijie Li <ZhijieLi@outlook.com>
2018-05-01 14:31:42 +0800

    P49

---
+++ LeetCode.Test/P49_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P49_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new P49();
+            t.GroupAnagrams(new string[] {"eat", "tea", "tan", "ate", "nat", "bat"});
+        }
+    }
+}
---
+++ LeetCode/P49.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P49
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dic = new Dictionary<string, IList<string>>();
+            foreach (string str in strs)
+            {
+                string sortedStr = Sort(str);
+
+                if (dic.ContainsKey(sortedStr))
+                {
+                    dic[sortedStr].Add(str);
+                }
+                else
+                {
+                    dic[sortedStr] = new List<string> { str };
+                }
+            }
+
+            return dic.Values.ToList();
+        }
+
+        private string Sort(string str)
+        {
+            char[] sorted = str.ToCharArray();
+            Array.Sort(sorted);
+            return new string(sorted);
+        }
+    }
+}
