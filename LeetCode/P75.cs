d83ed79558df19da8b52288cda80de9089ad8c33
Zhijie Li <ZhijieLi@outlook.com>
2018-05-02 18:26:55 +0800

    P75

---
+++ LeetCode/P75.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P75
+    {
+        public void SortColors(int[] nums)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (k < nums.Length)
+            {
+                if (nums[k] == 0)
+                {
+                    Swap(nums, i, k);
+                    i++;
+                    if (i > j) j = i;
+                    if (i > k) k = i;
+                }
+                else if (nums[k] == 1)
+                {
+                    Swap(nums, j, k);
+                    j++;
+                    if (j > k) k = j;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+        }
+
+        private void Swap(int[] nums, int i, int k)
+        {
+            var temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+        }
+    }
+}
