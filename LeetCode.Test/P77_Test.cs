3d64c9547eb50beb2d3169a4ee6ba47e3c10f3cf
Zhijie Li <ZhijieLi@outlook.com>
2018-05-02 23:44:22 +0800

    P77 Need Improved

---
+++ LeetCode.Test/P77_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P77_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t =new P77();
+            Console.WriteLine( t.Combine(4,2));
+
+        }
+    }
+}
---
+++ LeetCode/P77.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P77
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            bool[] ban = new bool[n + 1];
+            return Combine(n, k, ban);
+        }
+
+        private List<IList<int>> Combine(int n, int k, bool[] ban)
+        {
+            if (k == 0) return new List<IList<int>> { new List<int>() };
+            bool[] newBan = new bool[n+1];
+            ban.CopyTo(newBan,0);
+            var result = new List<IList<int>>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (newBan[i] == false)
+                {
+                    newBan[i] = true;
+                    Combine(n, k - 1, newBan).ForEach(item => result.Add(new List<int>(item) { i }));
+                }
+            }
+
+            return result;
+        }
+    }
+}
