5c724256fe250078bf47db8bb58b9886828a3164
Zhijie Li <ZhijieLi@outlook.com>
2018-05-02 15:53:46 +0800

    P71

---
+++ LeetCode/P71.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P71
+    {
+        public string SimplifyPath(string path)
+        {
+            var folders = new List<string>();
+            string[] pathList = path.Split('/');
+            foreach (string p in pathList)
+            {
+                if (p == "..")
+                {
+                    if (folders.Count > 0) folders.RemoveAt(folders.Count - 1);
+                }
+                else if (p == "." || p == "")
+                {
+                    continue;
+                }
+                else
+                {
+                    folders.Add(p);
+                }
+            }
+
+            if (folders.Count == 0)
+            {
+                return "/";
+            }
+
+            var result = string.Empty;
+            foreach (string folder in folders)
+            {
+                result += "/" + folder;
+            }
+
+            return result;
+        }
+    }
+}
