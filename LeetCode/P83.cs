d7ec9bf0e0837caa820ec2392a30b5eb9aa3ff29
Zhijie Li <ZhijieLi@outlook.com>
2018-05-05 16:30:33 +0800

    P83

---
+++ LeetCode/P83.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P83
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            var current = head;
+            var next = head.next;
+
+            while (current != null)
+            {
+                while (next != null && current.val == next.val)
+                {
+                    next = next.next;
+                }
+
+                current.next = next;
+                current = next;
+            }
+
+            return head;
+        }
+    }
+}
