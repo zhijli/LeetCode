d8b4e54c03d9937ef14ffcd4aba3511c6644c57e
Zhijie Li <ZhijieLi@outlook.com>
2018-05-06 22:05:09 +0800

    P78

--- LeetCode/P78.cs
+++ LeetCode/P78.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace LeetCode
 {
-    class P78
+    public class P78
     {
-        //public IList<IList<int>> Subsets(int[] nums)
-        //{
-        //}
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            var current = new List<int>();
+            Subsets(nums, 0, current, result);
+            return result;
+        }
 
+        private void Subsets(int[] nums, int index, List<int> current, List<IList<int>> result)
+        {
+            if (index == nums.Length)
+            {
+                result.Add(new List<int>(current));
+                return;
+            }
+
+            current.Add(nums[index]);
+            Subsets(nums, index + 1, current, result);
+            current.Remove(nums[index]);
+
+            Subsets(nums, index + 1, current, result);
+        }
     }
 }
