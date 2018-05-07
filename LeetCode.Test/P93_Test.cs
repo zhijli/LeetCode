df9f060b5cde4efe2175d60e0f9698c7f3f12553
Zhijie Li <ZhijieLi@outlook.com>
2018-05-07 22:36:46 +0800

    P93

---
+++ LeetCode.Test/P93_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class P93_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t= new P93();
+            t.RestoreIpAddresses("25525511135");
+        }
+    }
+}
--- LeetCode/P93.cs
+++ LeetCode/P93.cs
@@ -18,69 +18,111 @@ namespace LeetCode
         {
             if (s == null || s.Length < 4) return new List<string>();
 
-            var dp = new List<List<string>>[s.Length, 5];
-            for (int i = s.Length - 1; i >= 0; i--)
+            var dp = new List<List<string>>[s.Length + 1, 5];
+            for (int i = s.Length; i >= 0; i--)
             {
                 for (int j = 4; j >= 0; j--)
                 {
-                    if (s.Length - i < j || j == 0)
+                    if (s.Length == i)
+                    {
+                        if (j == 0)
+                        {
+                            dp[i, j] = new List<List<string>> { new List<string>() };
+                        }
+                        else
+                        {
+                            dp[i, j] = new List<List<string>>();
+                        }
+                    }
+                    else if (s.Length - i < j || j == 0)
                     {
                         dp[i, j] = new List<List<string>>();
                     }
+
                 }
             }
+            dp[s.Length - 1, 1] = new List<List<string>> { new List<string> { s.Substring(s.Length - 1, 1) } };
 
-            for (int i = s.Length - 1; i >= 0; i--)
+            for (int i = s.Length - 2; i >= 0; i--)
             {
-                for (int j = 4; j >= 0; j--)
+                for (int j = 4; j >= 1; j--)
                 {
                     if (s[i] == '0')
                     {
-                        var l = new List<List<string>>(dp[i + 1, j - 1]);
-                        l.ForEach(str => str.Add("0"));
+                        var l = new List<List<string>>();
+                        dp[i + 1, j - 1].ForEach(item => l.Add(new List<string>(item)));
+                        l.ForEach(str => str.Add(s.Substring(i, 1)));
                         dp[i, j] = l;
                     }
-
-                    if (s[i] == '1')
+                    else if (s[i] == '1')
                     {
-                        var l = new List<List<string>>(dp[i + 1, j - 1]);
-                        l.ForEach(str => str.Add("1"));
+                        var l = new List<List<string>>();
+                        dp[i + 1, j - 1].ForEach(item => l.Add(new List<string>(item)));
+                        l.ForEach(str => str.Add(s.Substring(i, 1)));
                         dp[i, j] = l;
 
-                        l = new List<List<string>>(dp[i + 2, j - 1]);
-                        l.ForEach(str => str.Add(s.Substring(i,2)));
-                        dp[i, j].AddRange(l);
+                        if (i <= s.Length - 2)
+                        {
+                            var temp = new List<List<string>>();
+                            dp[i + 2, j - 1].ForEach(item => temp.Add(new List<string>(item)));
+                            temp.ForEach(str => str.Add(s.Substring(i, 2)));
+                            dp[i, j].AddRange(temp);
+                        }
 
-                        l = new List<List<string>>(dp[i + 3, j - 1]);
-                        l.ForEach(str => str.Add(s.Substring(i, 3)));
-                        dp[i, j].AddRange(l);
+                        if (i <= s.Length - 3)
+                        {
+                            var temp = new List<List<string>>();
+                            dp[i + 3, j - 1].ForEach(item => temp.Add(new List<string>(item)));
+                            temp.ForEach(str => str.Add(s.Substring(i, 3)));
+                            dp[i, j].AddRange(temp);
+                        }
                     }
-
-                    if (s[i] == '2')
+                    else if (s[i] == '2')
                     {
-                        var l = new List<List<string>>(dp[i + 1, j - 1]);
-                        l.ForEach(str => str.Add("1"));
+                        var l = new List<List<string>>();
+                        dp[i + 1, j - 1].ForEach(item => l.Add(new List<string>(item)));
+                        l.ForEach(str => str.Add(s.Substring(i, 1)));
                         dp[i, j] = l;
 
-                        l = new List<List<string>>(dp[i + 2, j - 1]);
-                        l.ForEach(str => str.Add(s.Substring(i, 2)));
-                        dp[i, j].AddRange(l);
+                        if (i <= s.Length - 2 )
+                        {
+                            var temp = new List<List<string>>();
+                            dp[i + 2, j - 1].ForEach(item => temp.Add(new List<string>(item)));
+                            temp.ForEach(str => str.Add(s.Substring(i, 2)));
+                            dp[i, j].AddRange(temp);
+                        }
 
-                        l = new List<List<string>>(dp[i + 3, j - 1]);
-                        l.ForEach(str => str.Add(s.Substring(i, 3)));
-                        dp[i, j].AddRange(l);
+                        if (i <= s.Length - 3 && (s[i + 1] <= '4' ||  s[i + 1] == '5' && s[i + 2] <= '5'))
+                        {
+                            var temp = new List<List<string>>();
+                            dp[i + 3, j - 1].ForEach(item => temp.Add(new List<string>(item)));
+                            temp.ForEach(str => str.Add(s.Substring(i, 3)));
+                            dp[i, j].AddRange(temp);
+                        }
                     }
+                    else
+                    {
+                        var l = new List<List<string>>();
+                        dp[i + 1, j - 1].ForEach(item => l.Add(new List<string>(item)));
+                        l.ForEach(str => str.Add(s.Substring(i, 1)));
+                        dp[i, j] = l;
 
-                    dp[i, j] = dp[i + 1, j - 1] + dp[i + 2, j - 1] + dp[i + 3, j - 1];
+                        if (i <= s.Length - 2)
+                        {
+                            var temp = new List<List<string>>();
+                            dp[i + 2, j - 1].ForEach(item => temp.Add(new List<string>(item)));
+                            temp.ForEach(str => str.Add(s.Substring(i, 2)));
+                            dp[i, j].AddRange(temp);
+                        }
+                    }
                 }
             }
 
-            var result = new List<string>();
-            var current = new List<string>();
-
-
-
-            return result;
+            return dp[0, 4].Select(str =>
+            {
+                str.Reverse();
+                return String.Join(".", str);
+            }).ToList();
         }
     }
 }
