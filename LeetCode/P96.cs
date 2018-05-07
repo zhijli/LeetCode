d3ea28d251de3de78fc1e0d437fc23c4c629bd6c
Zhijie Li <ZhijieLi@outlook.com>
2018-05-08 00:59:11 +0800

    P96

---
+++ LeetCode/P96.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class P96
+    {
+        public int NumTrees(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
