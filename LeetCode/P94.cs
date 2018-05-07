12ae8d94469facf905c432d68a5058662d7ad37a
Zhijie Li <ZhijieLi@outlook.com>
2018-05-07 23:18:21 +0800

    P94

---
+++ LeetCode/P94.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class P94
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root == null) return result;
+            
+            var stack = new Stack<TreeNode>();
+
+            var current = root;
+
+            while (current != null || stack.Count!=0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+
+                current = stack.Pop();
+                result.Add(current.val);
+                current = current.right;
+            }
+
+            return result;
+        }
+    }
+}
