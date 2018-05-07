b176f3ddde94cd2f9c288a336a2ba85329a61e52
Zhijie Li <ZhijieLi@outlook.com>
2018-05-08 01:30:43 +0800

    P100

---
+++ LeetCode/P100.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class P100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == q) return true;
+            if (p == null) return false;
+            if (q == null) return false;
+
+            return (p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
+        }
+    }
+}
