67a7485ede6509c4e17cd5cf1ef36d52dbc03dd1
Zhijie Li <ZhijieLi@outlook.com>
2018-05-12 21:00:07 +0800

    P102

---
+++ LeetCode/P102.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P102
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(null);
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if (node == null)
+                {
+                    if (queue.Count > 0)
+                    {
+                        result.Add(new List<int>());
+                        queue.Enqueue(null);
+                    }
+                }
+                else
+                {
+                    result.Last().Add(node.val);
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+
+            return result;
+        }
+    }
+}
