731f3e30bab45731edbf308224e08dfa7d758e9b
Zhijie Li <ZhijieLi@outlook.com>
2018-05-12 22:32:30 +0800

    P104

---
+++ LeetCode/P104.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P104
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
