530ebea660b1cd83e1e290f80271feda2d8234f7
Zhijie Li <ZhijieLi@outlook.com>
2018-05-13 21:59:31 +0800

    P107

---
+++ LeetCode/P107.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P107
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(null);
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == null)
+                {
+                    if (queue.Count > 0)
+                    {
+                        queue.Enqueue(null);
+                        result.Add(new List<int>());
+                    }
+                }
+                else
+                {
+                    result.Last().Add(node.val);
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+
+            result.Reverse(0, result.Count);
+            return result;
+        }
+    }
+}
