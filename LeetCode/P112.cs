900ab8d79f4f8fff6be75fd69dc8869ab5de3c01
Zhijie Li <ZhijieLi@outlook.com>
2018-05-21 23:47:27 +0800

    P112

---
+++ LeetCode/P112.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P112
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null) return false;
+            if (root.left == null && root.right == null) return root.val == sum;
+            if (root.left == null) return HasPathSum(root.right, sum - root.val);
+            if (root.right == null) return HasPathSum(root.left, sum - root.val);
+
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+    }
+}
