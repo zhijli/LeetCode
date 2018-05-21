78cba95ddc0153b06f307870e84373a6bb38b964
Zhijie Li <ZhijieLi@outlook.com>
2018-05-21 17:47:18 +0800

    P111

---
+++ LeetCode/P111.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class P111
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            if (root.left == null) return MinDepth(root.right) + 1;
+
+            if (root.right == null) return MinDepth(root.left) + 1;
+
+            return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+        }
+    }
+}
