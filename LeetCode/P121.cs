c83897090ce1921d38c4b93d05479a6046ac1575
Zhijie Li <ZhijieLi@outlook.com>
2018-05-23 00:29:59 +0800

    Add P121 and P122

---
+++ LeetCode/P121.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0) return 0;
+            int min = prices[0];
+            int result = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+
+                if (result < prices[i] - min)
+                {
+                    result = prices[i] - min;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/P122.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P122
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var result = 0;
+            var hasCash = true;
+
+            var buyPrice = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (hasCash)
+                {
+                    if (prices[i] < prices[i + 1])
+                    {
+                        buyPrice = prices[i];
+                        hasCash = false;
+                    }
+                }
+                else
+                {
+                    if (prices[i] > prices[i + 1])
+                    {
+                        result += prices[i] - buyPrice;
+                        hasCash = true;
+                    }
+                }
+            }
+
+            if (!hasCash)
+            {
+                result += prices[prices.Length - 1] - buyPrice;
+            }
+
+            return result;
+        }
+    }
+}
