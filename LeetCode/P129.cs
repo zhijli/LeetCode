065bc173a30709607dc6551fa5e2e8c2762712a9
Zhijie Li <ZhijieLi@outlook.com>
2018-06-11 17:19:31 +0800

    P129

---
+++ LeetCode/P129.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class P129
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return SumNumbers(root, 0);
+        }
+
+        private int SumNumbers(TreeNode root, int current)
+        {
+            var val = current * 10 + root.val;
+            if (root.left == null && root.right == null) return val;
+
+            var result = 0;
+            if (root.left != null) result += SumNumbers(root.left, val);
+            if (root.right != null) result += SumNumbers(root.right, val);
+            return result;
+        }
+    }
+}
