6fb4625d8b5fab7c25b074502b5240b25c050bda
Zhijie Li <ZhijieLi@outlook.com>
2018-11-26 13:24:25 +0800

    P134

---
+++ LeetCode/P134.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P134
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            var val = 0;
+            var min = 0;
+            int minIndex = -1;
+            for (int i = 0; i < gas.Length; i++)
+            {
+                val = val + gas[i] - cost[i];
+                if (val <= min)
+                {
+                    min = val;
+                    minIndex = i;
+                }
+            }
+
+            if (val >= 0)
+            {
+                return (minIndex + 1) % gas.Length;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
