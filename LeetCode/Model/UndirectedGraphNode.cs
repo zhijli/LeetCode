e6986a1ee679c8f000b182c9a3d75b973c650be6
Zhijie Li <ZhijieLi@outlook.com>
2018-11-26 11:33:27 +0800

    P133

---
+++ LeetCode/Model/UndirectedGraphNode.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UndirectedGraphNode
+    {
+        public int label;
+        public IList<UndirectedGraphNode> neighbors;
+        public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+    };
+
+}
---
+++ LeetCode/P133.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using LeetCode.Model;
+
+    public class P133
+    {
+        public UndirectedGraphNode CloneGraph(UndirectedGraphNode node)
+        {
+            if (node == null) return null;
+
+            var dic = new Dictionary<UndirectedGraphNode, UndirectedGraphNode>();
+            var isVisited = new HashSet<UndirectedGraphNode>();
+            var queue = new Queue<UndirectedGraphNode>();
+
+            var result = GetOrAddGraphNodeMapping(dic, node);
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (!isVisited.Contains(current))
+                {
+                    var cloneNode = GetOrAddGraphNodeMapping(dic, current);
+
+                    foreach (UndirectedGraphNode neighbor in current.neighbors)
+                    {
+                        var cloneNeighbor = GetOrAddGraphNodeMapping(dic, neighbor);
+                        cloneNode.neighbors.Add(cloneNeighbor);
+                        queue.Enqueue(neighbor);
+                    }
+                    isVisited.Add(current);
+                }
+            }
+
+            return result;
+        }
+
+        private UndirectedGraphNode GetOrAddGraphNodeMapping(
+            Dictionary<UndirectedGraphNode, UndirectedGraphNode> dic,
+            UndirectedGraphNode node)
+        {
+            if (dic.ContainsKey(node))
+            {
+                return dic[node];
+            }
+            else
+            {
+                var newNode = new UndirectedGraphNode(node.label);
+                dic.Add(node, newNode);
+                return newNode;
+            }
+        }
+    }
+}
