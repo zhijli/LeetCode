f9aba175c75f7e9990fc0042d9290a51052436a6
Zhijie Li <ZhijieLi@outlook.com>
2018-11-30 10:53:50 +0800

    P136

---
+++ LeetCode/P136.cs
@@ -0,0 +1,24 @@
+﻿
+namespace LeetCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class P136
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+
+            return result;
+        }
+    }
+}
